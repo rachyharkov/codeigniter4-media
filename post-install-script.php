9b6a188670aab6143d33cb2ff902dca15449f2b5
Rachmad Nur Hayat <44340813+rachyharkov@users.noreply.github.com>
2023-10-14 22:53:45 +0700

    update

--- post-install-script.php
+++ post-install-script.php
@@ -1,13 +1,13 @@
 <?php
 
 echo 'Creating migration file...';
-$migrationFile = 'app/Database/Migrations/' . date('Y-m-d-His') . '_CreateMedia.php';
+$migrationFile = 'app/Database/Migrations/2023-08-01-092856_CreateMedia.php';
 
 echo 'Migration file: ' . $migrationFile;
-copy('table/CreateMedia.php', $migrationFile);
+copy(__DIR__ . '/table/2023-08-01-092856_CreateMedia.php', $migrationFile);
 
 echo 'Creating uploads directory...';
-mkdir('public/uploads');
+mkdir(__DIR__ . '/public/uploads');
 
 echo 'Creating no-image.png...';
-copy('assets/no-image.png', 'public/media/no-image.png');
+copy(__DIR__ . '/assets/no-image.png', __DIR__ . '/public/media/no-image.png');
